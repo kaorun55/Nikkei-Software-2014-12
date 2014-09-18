fda9f50b6db142885f61240088da78366978ac5f
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-09-19 00:53:51 +0900

    MVVMのサンプルを追加

---
+++ WPF/Mvvm/Mvvm/Model/LeapMotionController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Leap;
+
+namespace Mvvm.Model
+{
+    public class LeapMotionController : INotifyPropertyChanged
+    {
+        Controller leap = new Controller();
+
+        public LeapMotionController()
+        {
+            leap.SetPolicyFlags( Controller.PolicyFlag.POLICY_IMAGES );
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        void CompositionTarget_Rendering( object sender, EventArgs e )
+        {
+            var frame = leap.Frame();
+
+            var images = frame.Images;
+            if ( images.Count == 0 ) {
+                return;
+            }
+
+            ImageLeft = BitmapSource.Create( images[0].Width, images[0].Height, 96, 96, PixelFormats.Gray8, null, images[0].Data, images[0].Width );
+            NotifyPropertyChanged( "ImageLeft" );
+
+            ImageRight = BitmapSource.Create( images[1].Width, images[1].Height, 96, 96, PixelFormats.Gray8, null, images[1].Data, images[1].Width );
+            NotifyPropertyChanged( "ImageRight" );
+        }
+
+        public BitmapSource ImageRight
+        {
+            get;
+            set;
+        }
+
+        public BitmapSource ImageLeft
+        {
+            get;
+            set;
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged( [CallerMemberName] string propertyName = "" )
+        {
+            if ( PropertyChanged != null ) {
+                PropertyChanged( this, new PropertyChangedEventArgs( propertyName ) );
+            }
+        }
+        #endregion
+    }
+}
---
+++ WPF/Mvvm/Mvvm/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Mvvm.Model;
+
+namespace Mvvm.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        LeapMotionController leap = new LeapMotionController();
+
+        public MainWindowViewModel()
+        {
+            leap.PropertyChanged += leap_PropertyChanged;
+        }
+
+        void leap_PropertyChanged( object sender, PropertyChangedEventArgs e )
+        {
+            if ( e.PropertyName == "ImageRight" ) {
+                NotifyPropertyChanged( e.PropertyName );
+            }
+            else if ( e.PropertyName == "ImageLeft" ) {
+                NotifyPropertyChanged( e.PropertyName );
+            }
+        }
+
+        public BitmapSource ImageRight
+        {
+            get
+            {
+                return leap.ImageRight;
+            }
+        }
+
+        public BitmapSource ImageLeft
+        {
+            get
+            {
+                return leap.ImageLeft;
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged( [CallerMemberName] string propertyName = "" )
+        {
+            if ( PropertyChanged != null ) {
+                PropertyChanged( this, new PropertyChangedEventArgs( propertyName ) );
+            }
+        }
+        #endregion
+    }
+}
