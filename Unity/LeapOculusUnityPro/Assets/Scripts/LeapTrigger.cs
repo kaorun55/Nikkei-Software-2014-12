1ecaf36210ff8705cfd5d36ade5a085a1a78553f
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-09-24 00:56:31 +0900

    LeapOculusのサンプル

---
+++ Unity/LeapOculusUnityPro/Assets/Scripts/LeapTrigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeapTrigger : MonoBehaviour {
+
+    public AnimationClip clip;
+    Animator anim;
+
+    public float delayWeight;
+    float current = 0;
+    bool trigger = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( trigger ) {
+            current = 1;
+        }
+        else {
+            current = Mathf.Lerp( current, 0, delayWeight );
+        }
+
+        anim.SetLayerWeight( 1, current );
+    }
+
+    void OnTriggerEnter( Collider collider )
+    {
+        trigger = true;
+        anim.CrossFade( clip.name, 0 );
+    }
+
+    void OnTriggerExit( Collider other )
+    {
+        trigger = false;
+    }
+}
