a57de21fa3f5f99cdb7ec63ac09c8b5319dc6a32
Kaoru NAKAMURA <kaorun55@gmail.com>
2014-09-30 15:33:02 +0900

    Unity Free版のサンプルを追加

---
+++ Unity/LeapUnityFree/Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTrigger : MonoBehaviour {
+    public AnimationClip clip;
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // あたったときに、指定したアニメーションを再生する
+    void OnTriggerEnter( Collider collider )
+    {
+        anim.Play( clip.name );
+    }
+}
---
+++ Unity/LeapUnityFree/Assets/TinyGladiator/Documents/Readme.txt
@@ -0,0 +1,13 @@
+Tiny Gladiator
+==============
+
+Low-poly modeled.
+Rigged mecanim compatible.
+Package contains 3DMax file.
+Included side scroll demo with parallax background.
+
+Contact :
+- Homepage Link : http://buntgames.com
+- Blog Link : http://hompy.info
+- Facebook Link : http://facebook.com/buntgames
+- Email : hompy@buntgames.com
